8551f319c6cebdedb917248797f8c75e6d76504f
Vincent den Boer <vincent@shishkabab.net>
2020-04-12 20:53:45 +0200

    Initial commit

---
+++ ts/apps.ts
@@ -0,0 +1,8 @@
+import { CollectionDefinitionMap } from "@worldbrain/storex";
+
+export interface AppSchema {
+    collectionDefinitions?: CollectionDefinitionMap
+    collectionDescriptions?: any
+    virtualTables?: any
+    terms?: any
+}
---
+++ ts/plugins.ts
@@ -0,0 +1,18 @@
+import { StorexHubApi_v0 } from "./api";
+
+export interface PluginInfo {
+    identifier: string
+    version: string
+    description: string
+    apps: Array<{ name: string, description?: string }>
+    siteUrl: string
+    mainPath: string
+    entryFunction: string
+}
+
+export interface PluginInterface {
+    start(): Promise<void>
+    stop(): Promise<void>
+}
+
+export type PluginEntryFunction = (input: { api: StorexHubApi_v0 }) => Promise<PluginInterface>
